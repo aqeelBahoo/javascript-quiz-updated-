891bfd37ced3134bc4f632add6fd9c1cb52b7300
aqeelBahoo <humza180@gmail.com>
2018-01-03 21:21:32 +0500

    add function on next question

--- app/controllers/quizCtrl.js
+++ app/controllers/quizCtrl.js
@@ -1,17 +1,45 @@
 angular.module('javascript-quiz')
-    .controller('quizCtrl', quizController)
+    .controller('quizCtrl', quizController);
 
-quizController.$inject = ['DataService']
+quizController.$inject = ['DataService'];
 
 function quizController(DataService) {
     var vm = this;
     vm.DataService = DataService;
     vm.selectedAnswer = selectedAnswer;
+    vm.questionAnswered = questionAnswered;
+    vm.setActiveQuestion = setActiveQuestion;
+
     vm.activeQuestion = 0;
+    var numAnsweredQuestions = 0;
 
     function selectedAnswer(index) {
         DataService.quizQuestions[vm.activeQuestion].selected = index;
-        console.log(DataService.quizQuestions[vm.activeQuestion].selected)
+        console.log(DataService.quizQuestions[vm.activeQuestion].selected);
+    }
+
+    function questionAnswered() {
+        if (DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+            numAnsweredQuestions++;
+            if (numAnsweredQuestions >= DataService.quizQuestions.length) {
+                //finalize the quiz
+            }
+        }
+        setActiveQuestion();
+    }
+
+    function setActiveQuestion() {
+        var breakout = true;
+
+        while (breakout){
+            vm.activeQuestion = vm.activeQuestion < DataService.quizQuestions.length-1 ? ++vm.activeQuestion: 0;
+            if(DataService.quizQuestions[vm.activeQuestion].selected == null){
+                breakout = false;
+            }
+
+        }
+
+
     }
 
 }
