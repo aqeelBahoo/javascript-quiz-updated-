5f43f121e0869d99037debabe19d79e046676ff0
aqeelBahoo <humza180@gmail.com>
2018-01-03 20:52:23 +0500

    add style on button and questions answers

--- app/controllers/quizCtrl.js
+++ app/controllers/quizCtrl.js
@@ -6,6 +6,12 @@ quizController.$inject = ['DataService']
 function quizController(DataService) {
     var vm = this;
     vm.DataService = DataService;
+    vm.selectedAnswer = selectedAnswer;
     vm.activeQuestion = 0;
+
+    function selectedAnswer(index) {
+        DataService.quizQuestions[vm.activeQuestion].selected = index;
+        console.log(DataService.quizQuestions[vm.activeQuestion].selected)
+    }
 
 }
