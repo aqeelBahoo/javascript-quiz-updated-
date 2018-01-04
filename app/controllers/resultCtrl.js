aed0f06999b2a7f6ea1a9d0efa4807d024843eed
aqeelBahoo <humza180@gmail.com>
2018-01-04 21:53:38 +0500

    styling on result.html and controller

--- app/controllers/resultCtrl.js
+++ app/controllers/resultCtrl.js
@@ -5,7 +5,13 @@ function resultController(DataService, QuizMetrics) {
     var vm = this;
     vm.DataService = DataService;
     vm.QuizMetrics = QuizMetrics;
+    vm.selectQuestion = selectQuestion;
     vm.activeQuestion = 0;
+
+
+    function selectQuestion(index){
+        vm.activeQuestion = index;
+    }
 
 
 }
